7190b5c304a44b117eecbfcb93b09519b800c92c
Hyeoncheol <hyeoncheol.contact@gmail.com>
2023-11-06 15:49:12 +0900

    [EDIT] : 프로필 사진 변경

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile.jpeg"
+        src="../images/profile2.jpg"
         width={50}
         height={50}
         quality={95}
